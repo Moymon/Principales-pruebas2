f8a2a43632ebdad8c8bc3d8be3c5f4578cb0ab36
Moymon <47367207+Moymon@users.noreply.github.com>
2020-04-03 22:10:52 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.querySelector('#genera-poke').addEventListener('click',MuestraPokedex);
 document.querySelector('#genera-poke').addEventListener('touch',MuestraPokedex);
-
+/*document.querySelector('#Pokemon').addEventListener('click',Puchamon);
+document.querySelector('#Pokemon').addEventListener('touch',Puchamon);
+*/
 function MuestraPokedex(e)
 {
     e.preventDefault();
@@ -8,6 +10,7 @@ function MuestraPokedex(e)
     const Tipo = document.getElementById('Arquetipo');
     const TipoSeleccionado = Tipo.options[Tipo.selectedIndex].value;
     console.log(TipoSeleccionado);
+
     let url ='';
     url = 'pokedex.json';
     console.log(url);
@@ -19,36 +22,45 @@ function MuestraPokedex(e)
         if(this.status === 200)
         {
             const pokes = JSON.parse(this.responseText);
-            console.log(pokes);
+            /*console.log(pokes);*/
             let contenido ='';
             
             var i;
             for(i in pokes.pokemons)
             {
                 var tam = pokes.pokemons[i].type;
-                contenido += '<div class="textcenter">';
+                
                 if(TipoSeleccionado != 0)
                 {
                     if(tam[0] == TipoSeleccionado)
                     {
-                        console.log(pokes.pokemons[i].name);
+                        contenido += `<div class=" border textcenter id="${pokes.pokemons[i].name}" onclick="Unique('${pokes.pokemons[i].name}')">`;
+                        /*console.log(pokes.pokemons[i].name);*/
                         contenido+=`<p> CP  ${pokes.pokemons[i].cp} </p>`;
                         contenido+= `<img src="${pokes.pokemons[i].sprite} ">`;
                         contenido+=`<p> ${pokes.pokemons[i].name} </p>`;
+                        contenido+=` <div class="fondoblanco border textcenter"> 
+                        <div class="teal leng-box " style="width: 100%;"></div></div>`;
                     }
                     else if(tam[1] == TipoSeleccionado)
                         {
-                            console.log(pokes.pokemons[i].name);
+                            contenido += `<div class=" border textcenter id="${pokes.pokemons[i].name}" onclick="Unique('${pokes.pokemons[i].name}')">`;
+                            /*console.log(pokes.pokemons[i].name);*/
                             contenido+=`<p> CP  ${pokes.pokemons[i].cp} </p>`;
                             contenido+= `<img src="${pokes.pokemons[i].sprite} ">`;
                             contenido+=`<p> ${pokes.pokemons[i].name} </p>`;
+                            contenido+=` <div class="fondoblanco border textcenter"> 
+                            <div class="teal leng-box " style="width: 100%;"></div></div>`;
                         }
                 }else if(TipoSeleccionado == 0)
                 {
-                    console.log(pokes.pokemons[i].name);
+                    contenido += `<div class=" border textcenter id="${pokes.pokemons[i].name}" onclick="Unique('${pokes.pokemons[i].name}')">`;
+                    /*console.log(pokes.pokemons[i].name);*/
                     contenido+=`<p> CP  ${pokes.pokemons[i].cp} </p>`;
                     contenido+= `<img src="${pokes.pokemons[i].sprite} ">`;
                     contenido+=`<p> ${pokes.pokemons[i].name} </p>`;
+                    contenido+=` <div class="fondoblanco border textcenter"> 
+                    <div class="teal leng-box " style="width: 100%;"></div></div>`;
                 }
                 contenido += '</div>';
             }
@@ -57,4 +69,42 @@ function MuestraPokedex(e)
         }
     }
     xhr.send();
+}
+
+function Unique(e)
+{
+    /*console.log(e);*/
+
+    let url ='';
+    url = 'pokedex.json';
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',url,true);
+
+    xhr.onload= function()
+    {
+        if(this.status === 200)
+        {
+            const pokes = JSON.parse(this.responseText);
+            
+            let contenido ='<div class="border textcenter">';
+            
+            var i;
+            for(i in pokes.pokemons)
+            {
+                if(e == pokes.pokemons[i].name)
+                {
+                    contenido+=`<p> CP  ${pokes.pokemons[i].cp} </p>`;
+                    contenido+= `<img src="${pokes.pokemons[i].sprite} ">`;
+                    contenido+=`<p> ${pokes.pokemons[i].name} </p>`;
+                    contenido+=` <div class="teal leng-box textverde" style="width: 100%;">Hola</div></div>`;
+                    contenido+=`<p> ${pokes.pokemons[i].hp} / ${pokes.pokemons[i].hp} HP </p>`;
+                    contenido+=`<p> Attack ${pokes.pokemons[i].attack} / Defense ${pokes.pokemons[i].defense} </p>`;
+                }
+                contenido += '</div>';
+            }
+
+            document.getElementById('Unico').innerHTML = contenido;
+        }
+    }
+    xhr.send(); 
 }
